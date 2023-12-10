426cd897e02776e65a207e127d16620acd51bb4f
The-greenpanter <judpenaca@unal.edu.co>
2023-12-10 14:56:52 -0500

    Completado modulo 03 test aprovado

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -8,13 +8,13 @@ function esPositivo(num) {
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
    // Tu código:
-   let respuesta = "Es ";
+   var respuesta = "Es ";
    if(num < 0){
       respuesta += "negativo";
-   } if (num > 0 ){
+   } else if (num > 0 ){
       respuesta +=("positivo");
    }else {
-      respuesta = "false";
+      respuesta = false;
    }
    return respuesta;
 }
@@ -33,33 +33,42 @@ function obtenerSaludo(nombre) {
    // Toma el string "nombre" y concatena otra string en la cadena para que tome la siguiente forma:
    // Ejemplo: "Martin" ---> "Hola Martin!"
    // Tu código:
-   let str = "Hola" + nombre ;
+   let str = "Hola" + " " + nombre +"!";
    return str;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
    // Retornar el área de un rectángulo teniendo su altura y ancho.
    // Tu código:
-   let alto,ancho;
-   let area = (b*a)/2;
-return area
+   let area;
+   area = (alto*ancho);
+   return area;
 }
 
 function retornarPerimetro(lado) {
    // La función recibe como argumento la medida de un lado de un cuadrado.
    // Debes retornar su perímetro.
    // Tu código:
+   let perimetro = 4*lado;
+   return perimetro
 }
 
 function areaDelTriangulo(base, altura) {
    // Calcula el área de un triángulo y retorna el resultado.
    // Tu código:
+   let area;
+   area = (base*altura)/2;
+   return area;
+
 }
 
 function deEuroAdolar(euro) {
    // Supongamos que 1 euro equivale a 1.20 dólares.
    // Debes calcular el valor recibido como argumento pasándolo a dolares.
    // Tu código:
+   let dolar;
+   dolar = (euro*1.20);
+   return dolar;
 }
 
 function esVocal(letra) {
@@ -67,7 +76,19 @@ function esVocal(letra) {
    // Si el usuario ingresa un string de más de un caracter debes retornar el mensaje: "Dato incorrecto".
    // Si no es vocal, tambien debe retornar "Dato incorrecto".
    // Tu código:
+   if (typeof letra === 'string' && letra.length == 1 ){
+      const letraMinuscula = letra.toLowerCase();
+      if ('aeiou'.includes(letraMinuscula)) {
+         return "Es vocal";
+      } else {
+         return "Dato incorrecto";
+      }
+   } else {
+      return "Dato incorrecto";
+   }
 }
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
